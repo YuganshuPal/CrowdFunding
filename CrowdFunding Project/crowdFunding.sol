3986b554d396d46a9073eaf7daac67e1c0eaab8f
YuganshuPal <72740175+YuganshuPal@users.noreply.github.com>
2022-02-03 11:08:40 +0530

    Add files via upload

---
+++ CrowdFunding Project/crowdFunding.sol
@@ -0,0 +1,81 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 < 0.9.0;
+contract CrowdFunding{
+    mapping(address=>uint) public contributors;
+    address public manager;
+    uint public minContribution;
+    uint public target;
+    uint public deadline;
+    uint public raisedAmount;
+    uint public noOfContributors;
+    
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+
+    }
+    mapping(uint=>Request) public requests;
+    uint public numRequests;
+    constructor(uint _target,uint _deadline){
+        target = _target;
+        deadline = block.timestamp + _deadline; //deadline is in seconds
+        minContribution = 100 wei;
+        manager = msg.sender;
+    }
+
+    function sendEth() public payable{
+        require(block.timestamp < deadline,"Deadline has passed.");
+        require(msg.value >= minContribution,"Minimum Contribution is not met.");
+        if(contributors[msg.sender]==0) //To check for multiple contributions by the same contributor
+        {
+            noOfContributors++;
+        }
+        contributors[msg.sender]+=msg.value;
+        raisedAmount+=msg.value;
+
+    }
+    function getContractBalance() public view returns(uint)
+    {
+        return address(this).balance;
+    }
+    function refund() public{
+        require(block.timestamp > deadline && raisedAmount < target,"You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user = payable(msg.sender); 
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+    }
+    modifier onlyManager(){
+        require(msg.sender == manager,"Only manager can access this");
+        _;
+    }
+    function createRequests(string memory _description,address payable _recipient,uint _value) public onlyManager{
+        Request storage newRequest = requests[numRequests]; // a structure using a mapping always reuires storage
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+    function voteRequest(uint _requestNo) public{
+        require(contributors[msg.sender] > 0 ,"You must be a contributor");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender]==false,"You have already voted");
+        thisRequest.voters[msg.sender]=true;
+        thisRequest.noOfVoters++;
+    }
+    function makePayment(uint _requestNo) public onlyManager{
+        require(raisedAmount >= target);
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed==false,"The request has been completed");
+        require(thisRequest.noOfVoters > noOfContributors/2,"Majority does not support");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed=true; 
+    }
+
+}
